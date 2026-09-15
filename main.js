e36321f79b8ff3602a332b153801c9359082b941
MariaNava17 <maria.nava@escola.pr.gov.br>
2026-09-15 11:39:39 -0300

    15.09

---
+++ main.js
@@ -0,0 +1,43 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#%*?,.';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if(tamanhoSenha>1) {
+        //tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha()
+}
+
+function aumentaTamanho() {
+    if(tamanhoSenha<20) {
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+for(i=0; <checkbox.length;i++){
+    checkbox[i].onclick = geraSenha;
+}
+geraSenha();
+function geraSenha(
+    let alfabeto = '';
+if(checkbox[0].cheked){
+    alfabeto = alfabeto + letrasMaiusculas;
+}
+if(checkbox[1].cheked)
+)
